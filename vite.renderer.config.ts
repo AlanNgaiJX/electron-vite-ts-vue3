84b06912fe33a9689652ad3360b28686bebf2b6a
AlanNgai <43108526+AlanNgaiJX@users.noreply.github.com>
2024-04-19 14:37:23 +0800

    init

---
+++ vite.renderer.config.ts
@@ -0,0 +1,40 @@
+import type { ConfigEnv, UserConfig } from "vite";
+import { defineConfig } from "vite";
+import { pluginExposeRenderer } from "./vite.base.config";
+
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
+import autoprefixer from "autoprefixer";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+
+// https://vitejs.dev/config
+export default defineConfig((env) => {
+  const forgeEnv = env as ConfigEnv<"renderer">;
+  const { root, mode, forgeConfigSelf } = forgeEnv;
+  const name = forgeConfigSelf.name ?? "";
+
+  return {
+    root,
+    mode,
+    base: "./",
+    build: {
+      outDir: `.vite/renderer/${name}`,
+    },
+    plugins: [pluginExposeRenderer(name), vue()],
+    resolve: {
+      preserveSymlinks: true,
+      alias: {
+        "@": resolve(__dirname, "../src"),
+        "~@": resolve(__dirname, "../src"),
+      },
+      extensions: [".mjs", ".js", ".tsx", ".json", ".sass", ".scss", ".ts"],
+    },
+    css: {
+      postcss: {
+        plugins: [autoprefixer],
+      },
+    },
+    clearScreen: false,
+  } as UserConfig;
+});
